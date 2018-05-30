21ac889e93dbba25464cc45a841c96c4006454f0
Cyclia <michael@xuberance.works>
2018-05-30 14:54:53 +0200

    Upload for help with local files

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,18 @@
+// TODO: Images don't work
+
 var express = require('express');
 var router = express.Router();
+
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'AllGrounds' });
 });
+
+/* GET stadium / ALS */
+router.get('/stadions/abelenstrastadion', function(req, res, next) {
+  res.render('abelenstrastadion', { title: 'Abe Lenstra Stadion Heerenveen' });
+});
+
 
 module.exports = router;
